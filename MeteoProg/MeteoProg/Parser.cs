69232711c648502e1f1c9c8341cc6533fe453144
Holofiber <aminus2010@gmail.com>
2018-03-06 00:13:08 +0200

    MP :: minor update

--- MeteoProg/MeteoProg/Parser.cs
+++ MeteoProg/MeteoProg/Parser.cs
@@ -19,6 +19,7 @@ namespace MeteoProg
 
         public void Pars()
         {
+            var todayMorning = new Sinoptic();
             var sinopticLviv = @"https://ua.sinoptik.ua/погода-львів";
 
             HtmlWeb web = new HtmlWeb();
@@ -28,12 +29,16 @@ namespace MeteoProg
             var currenTemperature = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"bd1c\"]/div[1]/div[1]/div[1]/p[2]");
             var minTemerature = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"bd1\"]/div[2]/div[1]/span");
             var maxTemerature = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"bd1\"]/div[2]/div[2]/span");
+            var press = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"bd1c\"]/div[1]/div[2]/table/tbody/tr[5]/td[4]");
 
-            var pattern = @"[+-]\d\d";
 
-            _sinoptic.TemperatureCurrent = RegexData(currenTemperature.InnerText, pattern);
+            
+            var pattern = @"[+-]\d{1,2}";
+
+            _sinoptic.TemperatureCurrent = RegexData(currenTemperature.InnerText, pattern); 
             _sinoptic.TemperatureMin = RegexData(minTemerature.InnerText, pattern);
             _sinoptic.TemperatureMax = RegexData(maxTemerature.InnerText, pattern);
+            _sinoptic.Pressure = Convert.ToInt32(press.InnerText);
 
         }
 
