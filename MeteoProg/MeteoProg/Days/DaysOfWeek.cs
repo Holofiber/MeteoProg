a8b29ebea5055251d273d2871b925438ad7207a6
Holofiber <aminus2010@gmail.com>
2018-03-24 15:02:06 +0200

    MP :: small fix

--- MeteoProg/MeteoProg/Days/DaysOfWeek.cs
+++ MeteoProg/MeteoProg/Days/DaysOfWeek.cs
@@ -21,6 +21,11 @@
             var day3 = _parser.Pars(3);
             var day4 = _parser.Pars(4);
             var day5 = _parser.Pars(5);
+            var day6 = _parser.Pars(6);
+            var day7 = _parser.Pars(7);
+            var day8 = _parser.Pars(8);
+            var day9 = _parser.Pars(9);
+            var day10 = _parser.Pars(10);
         }
     }
 }
--- MeteoProg/MeteoProg/Parser.cs
+++ MeteoProg/MeteoProg/Parser.cs
@@ -19,25 +19,27 @@ namespace MeteoProg
 
         public Sinoptic Pars(int dayId )
         {
-            var sinopticLviv = @"https://ua.sinoptik.ua/погода-львів";
+            var sinopticLviv = @"https://ua.sinoptik.ua/погода-львів/10-днів";
             HtmlWeb web = new HtmlWeb();
             var htmlDoc = web.Load(sinopticLviv);
 
             var currenTemperature = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"bd1c\"]/div[1]/div[1]/div[1]/p[2]");
             var minTemerature = htmlDoc.DocumentNode.SelectSingleNode($"//*[@id=\"bd{dayId}\"]/div[2]/div[1]/span");
             var maxTemerature = htmlDoc.DocumentNode.SelectSingleNode($"//*[@id=\"bd{dayId}\"]/div[2]/div[2]/span");
-            var press = htmlDoc.DocumentNode.SelectSingleNode($"//*[@id=\"bd{dayId}c\"]/div[1]/div[2]/table/tbody/tr[5]/td[4]");
-
-
+            var press = htmlDoc.DocumentNode.SelectSingleNode($"//*[@id=\"bd1c\"]/div[1]/div[2]/table/tbody/tr[5]/td[3]");
+            
             var pattern = @"[+-]\d{1,2}";
 
             _sinoptic.TemperatureCurrent = RegexData(currenTemperature.InnerText, pattern);
-            _sinoptic.TemperatureMin = RegexData(minTemerature.InnerText, pattern);
+             _sinoptic.TemperatureMin = RegexData(minTemerature.InnerText, pattern);
             _sinoptic.TemperatureMax = RegexData(maxTemerature.InnerText, pattern);
-            _sinoptic.Pressure = Convert.ToInt32(press.InnerText);
+            _sinoptic.Pressure = 100;//Convert.ToInt32(press.InnerText);
             _sinoptic.Humidity = 0;
             _sinoptic.Weather = "";
-            
+            //*[@id="bd8"]/div[2]/div[1]/span
+
+            //*[@id="bd2"]/div[1]
+            //*[@id="bd5"]/div[1]
 
             return _sinoptic;
         }
